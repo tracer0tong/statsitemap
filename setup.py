c52b5da9492d23e553798852cba66b6583b55f95
Yury Leonychev <yuriy.leonychev@gmail.com>
2015-04-22 00:45:59 +0300

    Update in setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-    name='sitemap',
-    packages=['sitemap'],
+    name='statsitemap',
+    packages=['statsitemap'],
     version='0.1',
     description='Library for building statistical graph (sitemap) from called URI/referer pairs',
     author='Yury Leonychev (@tracer0tong)',
     author_email='yuriy.leonychev@gmail.com',
     url='https://github.com/tracer0tong/statsitemap',
-    download_url='https://github.com/tracer0tong/sitemap/tarball/0.1',
-    keywords=['sitemap', 'graph', 'nginx', 'apache', 'accesslog'],
+    download_url='https://github.com/tracer0tong/statsitemap/tarball/0.1',
+    keywords=['statsitemap', 'graph', 'nginx', 'apache', 'accesslog'],
     classifiers=[],
 )
